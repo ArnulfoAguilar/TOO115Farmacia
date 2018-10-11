11a5f945d6afade77c6f4aae9a71f812e5779c0d
Kendal Sosa <kendal.alfonso37@gmail.com>
2018-10-10 23:07:10 -0600

    Cambio en la forma de servir archivos estaticos en el proyecto

--- TOO115Farmacia/urls.py
+++ TOO115Farmacia/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('farmacia/',include('apps.Farmacia.urls')),
     path('', RedirectView.as_view(url='/farmacia/index/'), name='index'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
