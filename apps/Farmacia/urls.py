c376aef194fbd7079a39f069751ab258728c318c
ArnulfoAguilar <arnulfo.aguilar9405@gmail.com>
2018-12-02 16:36:43 -0600

    comentando codigo de vistas Descuento

--- apps/Farmacia/urls.py
+++ apps/Farmacia/urls.py
@@ -62,8 +62,10 @@ urlpatterns = [
     path("Descuento/List", views.DescuentoList.as_view(), name="descuento_List"),
      # Agregar nuevo descuento
     path("Descuento/Create", views.DescuentoCreate.as_view(), name="descuento_Create"),
-      # Update descuento
+      # Actualizar descuento
     path("Descuento/Update/<int:pk>", views.DescuentoUpdate.as_view(), name="descuento_Update"),
+     # Eliminar descuento
     path("Descuento/Delete/<int:pk>", views.DescuentoDelete.as_view(), name="descuento_Delete"),
+     # Detalles descuento
      path("Descuento/Detail/<int:pk>", views.DescuentoDetail.as_view(), name="descuento_Detail"),
 ]
